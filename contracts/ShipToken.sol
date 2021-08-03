5227ecb57280213fb79b295baf6dc2d483a530b3
Ben Sparks <bensparks0@gmail.com>
2021-08-03 23:19:15 +0200

    Finished SHIP token

--- contracts/ShipToken.sol
+++ contracts/ShipToken.sol
@@ -3,14 +3,14 @@
 pragma solidity 0.8.6;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
-contract ShipToken is ERC20 {
-
+contract ShipToken is ERC20, ERC20Permit {
     constructor(
-        string memory _name,
-        string memory _symbol
-    ) ERC20(_name, _symbol) {
-        
+        string memory name_,
+        string memory symbol_,
+        uint256 totalSupply_
+    ) ERC20(name_, symbol_) ERC20Permit(name_) {
+        _mint(msg.sender, totalSupply_);
     }
-
 }
