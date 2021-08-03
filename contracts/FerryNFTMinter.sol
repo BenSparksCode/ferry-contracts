29c106d7e135139de5eb0eef834855f380151d00
Ben Sparks <bensparks0@gmail.com>
2021-08-03 20:13:59 +0200

    Added FerryNFTMinter skeleton contract

---
+++ contracts/FerryNFTMinter.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.6;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+// Integrates with Chainlink's VRF to generate truly unique NFTs with random numbers
+// Integrates with Zora to mint the NFTs
+
+contract FerryNFTMinter {
+    constructor() {}
+}
