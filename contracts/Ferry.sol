2313f8f651d51228b1dfaaef2f63c8bafcc35232
Ben Sparks <bensparks0@gmail.com>
2021-08-03 23:34:55 +0200

    Added initial functions and vars to Ferry contract

--- contracts/Ferry.sol
+++ contracts/Ferry.sol
@@ -3,6 +3,8 @@
 pragma solidity 0.8.6;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 
 // CORE:
 // Owned by a multi-sig wallet
@@ -15,6 +17,31 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 // LATER:
 // Payments in DAI/USDC/MATIC with SushiSwap convert to DAI
 
-contract Ferry {
-    constructor() {}
+contract Ferry is Ownable {
+
+    uint256 public monthlyFee; // monthly pro fee
+
+    // address => membership expiry timestamp
+    mapping(address => uint256) public memberships;
+
+    constructor(uint256 _monthlyFee) {
+        monthlyFee = _monthlyFee;
+    }
+
+
+    function paySubscription(address _account, uint256 _amount) public {
+
+    }
+
+
+    // Deposits DAI into Aave to earn interest
+    function depositInAave(uint256 _amount) public onlyOwner {
+        
+    }
+
+    // Withdraws DAI from Aave
+    function withdrawFromAave(uint256 _amount) public onlyOwner {
+        
+    }
+
 }
