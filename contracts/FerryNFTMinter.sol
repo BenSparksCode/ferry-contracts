716c612b9aa752bf0ea5fe8d078b5661834fc0ac
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2021-08-06 21:04:58 +0200

    Added Chainlink to NFT Minter

--- contracts/FerryNFTMinter.sol
+++ contracts/FerryNFTMinter.sol
@@ -3,29 +3,77 @@
 pragma solidity 0.8.6;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 import "./interfaces/IZoraMedia.sol";
 
 // Integrates with Chainlink's VRF to generate truly unique NFTs with random numbers
 // Integrates with Zora to mint the NFTs
 
-contract FerryNFTMinter {
+contract FerryNFTMinter is VRFConsumerBase, Ownable {
     address public minter;
     IMedia ZoraMedia;
 
-    constructor(address _zoraMediaAddress) {
+    // Chainlink vars
+    bytes32 internal keyHash;
+    uint256 internal fee;
+    uint256 public randomResult;
+
+    constructor(
+        address _zoraMediaAddress,
+        address _vrfCoordinator,
+        address _link,
+        bytes32 _keyHash,
+        uint256 _fee
+    )
+        VRFConsumerBase(
+            _vrfCoordinator, // VRF Coordinator
+            _link // LINK Token
+        )
+    {
         ZoraMedia = IMedia(_zoraMediaAddress);
+        // Chainlink setup
+        keyHash = _keyHash;
+        fee = _fee;
     }
 
-    function mint() external onlyMinter{
+    function mint() external onlyMinter {
         // TODO
         // build SVG NFT
         // populate basic data and 0 for bidshares
-
         // ZoraMedia.mint(data, bidShares);
     }
 
-    modifier onlyMinter {
+    modifier onlyMinter() {
         require(minter == msg.sender);
         _;
+    }
+
+    // ==================== //
+    //      CHAINLINK       //
+    // ==================== //
+
+    event RequestedRandomness(bytes32 requestId);
+
+    // TODO shouldn't be public, haters will drain the linkies
+    // Request random number
+    function getRandomNumber() public returns (bytes32 requestId) {
+        requestId = requestRandomness(keyHash, fee);
+        emit RequestedRandomness(requestId);
+    }
+
+    // Recieve random number callback result
+    function fulfillRandomness(bytes32 requestId, uint256 randomness)
+        internal
+        override
+    {
+        randomResult = randomness;
+    }
+
+    function withdrawLink() external onlyOwner {
+        require(
+            LINK.transfer(msg.sender, LINK.balanceOf(address(this))),
+            "Unable to transfer"
+        );
     }
 }
