b5c8989d57865bb9ce29c49e92f4736f900cb2ab
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2021-08-08 12:29:00 +0200

    Importing libs for deploy and verify script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,12 @@
-const hre = require("hardhat");
+import hre, { ethers } from "hardhat";
+import "@nomiclabs/hardhat-etherscan";
+import chalk from "chalk";
+import { Contract } from "ethers";
+import ProgressBar from "progress";
 
 const { constants } = require("../test/TestConstants")
+
+const verifiableNetwork = ["mainnet", "ropsten", "rinkeby", "goerli", "kovan", "polygon", "mumbai"];
 
 async function main() {
 
