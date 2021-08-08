3a4906f1fbdb28528ea8728926debe4430d129d3
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2021-08-08 15:23:30 +0200

    Prepping to deploy on Polygon mainnet for verification

--- hardhat.config.js
+++ hardhat.config.js
@@ -39,8 +39,8 @@ module.exports = {
       url: "https://rpc-mumbai.maticvigil.com",
       // url: "https://polygon-mumbai.g.alchemy.com/v2/" + process.env.ALCHEMY_API,
       accounts: [`${process.env.MUMBAI_DEPLOYER_PRIV_KEY}`],
-      gas: 2000000, //2 mil
-      gasPrice: 5000000000, //5 gwei
+      // gas: 2000000, //2 mil
+      // gasPrice: 5000000000, //5 gwei
       chainId: 80001
     },
     polygon: {
